227585fb0d8625676090da94f82e27e94aa50d3c
Anwesh43 <anweshthecool0@gmail.com>
2024-06-17 15:04:33 +0530

    linedividerotupview: adding static creae method

--- linedividerotupview/src/main/java/com/example/linedividerotupview/LineDivideRotUpView.kt
+++ linedividerotupview/src/main/java/com/example/linedividerotupview/LineDivideRotUpView.kt
@@ -212,4 +212,12 @@ class LineDivideRotUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : LineDivideRotUpView {
+            val view : LineDivideRotUpView = LineDivideRotUpView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
