c5445691a213d431e90db661d126e51c214dd498
Anwesh43 <anweshthecool0@gmail.com>
2024-06-17 14:55:28 +0530

    linedividerotupview: adding LDRUNode class

--- linedividerotupview/src/main/java/com/example/linedividerotupview/LineDivideRotUpView.kt
+++ linedividerotupview/src/main/java/com/example/linedividerotupview/LineDivideRotUpView.kt
@@ -123,4 +123,45 @@ class LineDivideRotUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDRUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDRUNode? = null
+        private var prev : LDRUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDRUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDRUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDRUNode {
+            var curr : LDRUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
