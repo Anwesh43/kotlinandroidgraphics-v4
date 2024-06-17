009588570765eea86d4566ae3e8cb0b5eebf4019
Anwesh43 <anweshthecool0@gmail.com>
2024-06-17 15:20:18 +0530

    linedividerotupview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -12,36 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.graphicskotlinproject4.ui.theme.GraphicsKotlinProject4Theme
+import com.example.linedividerotupview.LineDivideRotUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            GraphicsKotlinProject4Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GraphicsKotlinProject4Theme {
-        Greeting("Android")
+        //enableEdgeToEdge()
+        LineDivideRotUpView.create(this)
     }
 }
