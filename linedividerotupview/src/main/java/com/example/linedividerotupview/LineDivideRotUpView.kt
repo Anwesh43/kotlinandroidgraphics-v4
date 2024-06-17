50f37d2a5a5f8e29b352bbfad7532ec557878a6b
Anwesh43 <anweshthecool0@gmail.com>
2024-06-17 13:52:01 +0530

    linedividerotupview: adding Canvas extension methods

--- linedividerotupview/src/main/java/com/example/linedividerotupview/LineDivideRotUpView.kt
+++ linedividerotupview/src/main/java/com/example/linedividerotupview/LineDivideRotUpView.kt
@@ -23,7 +23,40 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+val lines : Int = 4
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineDivideRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        val gap : Float = size / (lines - 1)
+        for (j in 0..(lines + 1)) {
+            drawXY(-(w / 2) * (1 - dsc(1).divideScale(j, lines)), -size + gap * j) {
+                drawLine(0f, 0f, -size, 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLDRUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+}
