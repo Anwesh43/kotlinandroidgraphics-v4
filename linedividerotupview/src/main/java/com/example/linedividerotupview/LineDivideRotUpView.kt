62c8a14300722464d21534262c1fa0f3b23ad605
Anwesh43 <anweshthecool0@gmail.com>
2024-06-17 13:53:12 +0530

    linedividerotupview: adding LineDivideRotUpView class

--- linedividerotupview/src/main/java/com/example/linedividerotupview/LineDivideRotUpView.kt
+++ linedividerotupview/src/main/java/com/example/linedividerotupview/LineDivideRotUpView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLDRUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
 }
+
+class LineDivideRotUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
