c05acca7a1ff89f01dcb1ad7aad333a01d5f8690
Anwesh43 <anweshthecool0@gmail.com>
2024-06-20 14:03:38 +0530

    linerottoarcview: adding Canvas extension methods

--- linerottoarcview/src/main/java/com/example/linerottoarcview/LineToArcRotView.kt
+++ linerottoarcview/src/main/java/com/example/linerottoarcview/LineToArcRotView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,37 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineToArcRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        drawXY(-size, 0f) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(3))
+            drawArc(RectF(-size / 2, 0f, size / 2, size), -90f, 180f * dsc(2), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawLTARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineToArcRot(scale, w, h, paint)
+}
