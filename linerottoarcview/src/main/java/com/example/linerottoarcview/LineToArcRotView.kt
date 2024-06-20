b04a6fa464262065ef8cd706f90fd081da4cf9f6
Anwesh43 <anweshthecool0@gmail.com>
2024-06-20 14:59:43 +0530

    linerottoarcview: adding LTARNode class

--- linerottoarcview/src/main/java/com/example/linerottoarcview/LineToArcRotView.kt
+++ linerottoarcview/src/main/java/com/example/linerottoarcview/LineToArcRotView.kt
@@ -124,4 +124,45 @@ class LineToArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LTARNode? = null
+        private var prev : LTARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTARNode {
+            var curr : LTARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
