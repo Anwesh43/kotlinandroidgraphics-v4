6051efb79666da83c6850fc99c92ead3912556a8
Anwesh43 <anweshthecool0@gmail.com>
2024-06-20 14:22:14 +0530

    linerottoarcview: adding LineToArcRotView class

--- linerottoarcview/src/main/java/com/example/linerottoarcview/LineToArcRotView.kt
+++ linerottoarcview/src/main/java/com/example/linerottoarcview/LineToArcRotView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLTARNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineToArcRot(scale, w, h, paint)
 }
+
+class LineToArcRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
