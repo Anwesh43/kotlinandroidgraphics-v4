06d177ce50af67c97f8cd782a077f6016105f2da
Anwesh43 <anweshthecool0@gmail.com>
2024-06-21 19:19:27 +0530

    bilinedroprotview: adidng BiLineDropRotView class

--- bilinedroprotview/src/main/java/com/example/bilinedroprotview/BiLineDropRotView.kt
+++ bilinedroprotview/src/main/java/com/example/bilinedroprotview/BiLineDropRotView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawBLDRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineDropRot(scale, w, h, paint)
 }
+
+class BiLineDropRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
