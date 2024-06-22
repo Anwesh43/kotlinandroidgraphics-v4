0fcc6108ccea34cb9e2ba0204700f69fef95dcb1
Anwesh43 <anweshthecool0@gmail.com>
2024-06-22 12:36:42 +0530

    bisectlinesweeprightview: adding BLSRNode class

--- bisectlinesweeprightview/src/main/java/com/example/bisectlinesweeprightview/BisectLineSweepRightView.kt
+++ bisectlinesweeprightview/src/main/java/com/example/bisectlinesweeprightview/BisectLineSweepRightView.kt
@@ -130,4 +130,45 @@ class BisectLineSweepRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLRSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLRSNode? = null
+        private var prev : BLRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLRSNode {
+            var curr : BLRSNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
