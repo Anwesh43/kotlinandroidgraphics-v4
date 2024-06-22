9bd54bb358b0817d619106d68f7881e48a8cd29f
Anwesh43 <anweshthecool0@gmail.com>
2024-06-22 12:18:18 +0530

    bisectlinesweeprightview: adding BisectLineSweepRightView class

--- bisectlinesweeprightview/src/main/java/com/example/bisectlinesweeprightview/BisectLineSweepRightView.kt
+++ bisectlinesweeprightview/src/main/java/com/example/bisectlinesweeprightview/BisectLineSweepRightView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawBLSRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBisectLineSweepRight(scale, w, h, paint)
 }
+
+class BisectLineSweepRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
