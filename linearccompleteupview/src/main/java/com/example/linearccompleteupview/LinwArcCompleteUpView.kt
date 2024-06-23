254030fc224cad66c852f605a38178328510226e
Anwesh43 <anweshthecool0@gmail.com>
2024-06-23 13:14:52 +0530

    linearccompleteupview: adding Renderer class

--- linearccompleteupview/src/main/java/com/example/linearccompleteupview/LinwArcCompleteUpView.kt
+++ linearccompleteupview/src/main/java/com/example/linearccompleteupview/LinwArcCompleteUpView.kt
@@ -199,4 +199,27 @@ class LineArcCompleteUpView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineArcCompleteUpView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lacu : LineArcCompleteUp = LineArcCompleteUp(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lacu.draw(canvas, paint)
+            animator.animate {
+                lacu.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lacu.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
