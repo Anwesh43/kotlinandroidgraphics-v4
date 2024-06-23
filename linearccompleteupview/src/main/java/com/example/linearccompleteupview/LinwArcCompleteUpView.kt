7ff4952c8d842443f9ffc86279aae9c6c7a7f00a
Anwesh43 <anweshthecool0@gmail.com>
2024-06-23 13:11:58 +0530

    linearccompleteupview: adding LineArcCompleteUp class

--- linearccompleteupview/src/main/java/com/example/linearccompleteupview/LinwArcCompleteUpView.kt
+++ linearccompleteupview/src/main/java/com/example/linearccompleteupview/LinwArcCompleteUpView.kt
@@ -176,4 +176,27 @@ class LineArcCompleteUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineArcCompleteUp(var i : Int) {
+
+        private var curr : LACUNode = LACUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
