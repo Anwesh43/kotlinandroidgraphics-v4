94db457d9987a4ddf9f14de998c74041f7584a86
Anwesh43 <anweshthecool0@gmail.com>
2024-06-27 09:59:26 +0530

    linefromdownupview: adding LFDUNode class

--- linefromdownupview/src/main/java/com/example/linefromdownupview/LineFromDownUpView.kt
+++ linefromdownupview/src/main/java/com/example/linefromdownupview/LineFromDownUpView.kt
@@ -120,4 +120,45 @@ class LineFromDownUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LFDUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LFDUNode? = null
+        private var prev : LFDUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LFDUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLFDUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LFDUNode {
+            var curr : LFDUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
