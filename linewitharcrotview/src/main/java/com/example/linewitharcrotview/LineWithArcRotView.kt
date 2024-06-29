ae1314d7f3c4d2609040018cd690b545fc212743
Anwesh43 <anweshthecool0@gmail.com>
2024-06-29 13:25:01 +0530

    linewitharcrotview: adding fixes

--- linewitharcrotview/src/main/java/com/example/linewitharcrotview/LineWithArcRotView.kt
+++ linewitharcrotview/src/main/java/com/example/linewitharcrotview/LineWithArcRotView.kt
@@ -56,6 +56,7 @@ fun Canvas.drawLWARNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawLineWithArcRot(scale, w, h, paint)
 }
 
