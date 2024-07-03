ea928f74d8420709df32ba76269c7fe89783cd23
Anwesh43 <anweshthecool0@gmail.com>
2024-07-03 10:29:35 +0530

    linepartrotshiftview: adding LinePartRotShiftView class

--- linepartrotshiftview/src/main/java/com/example/linepartrotshiftview/LinePartRotShiftView.kt
+++ linepartrotshiftview/src/main/java/com/example/linepartrotshiftview/LinePartRotShiftView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLPRSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLinePartRotShift(w, h, scale, paint)
 }
+
+class LinePartRotShiftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
