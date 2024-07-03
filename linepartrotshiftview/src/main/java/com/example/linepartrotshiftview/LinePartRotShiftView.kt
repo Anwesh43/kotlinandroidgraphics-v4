a8287d23f716c81bdbd58ed0c7b574394108d0e4
Anwesh43 <anweshthecool0@gmail.com>
2024-07-03 23:54:32 +0530

    linepartrotshiftview: adding Renderer class

--- linepartrotshiftview/src/main/java/com/example/linepartrotshiftview/LinePartRotShiftView.kt
+++ linepartrotshiftview/src/main/java/com/example/linepartrotshiftview/LinePartRotShiftView.kt
@@ -183,4 +183,27 @@ class LinePartRotShiftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LinePartRotShiftView) {
+
+        private val animator : Animator = Animator(view)
+        private val lprs : LinePartRotShift = LinePartRotShift(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lprs.draw(canvas, paint)
+            animator.animate {
+                lprs.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lprs.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
