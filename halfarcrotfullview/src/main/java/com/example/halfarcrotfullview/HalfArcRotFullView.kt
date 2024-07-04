074fc8cb20639c3fc89dcfcd33ab431965422ac3
Anwesh43 <anweshthecool0@gmail.com>
2024-07-04 14:22:49 +0530

    halfarcrotfullview: adding HalfArcRotFullView class

--- halfarcrotfullview/src/main/java/com/example/halfarcrotfullview/HalfArcRotFullView.kt
+++ halfarcrotfullview/src/main/java/com/example/halfarcrotfullview/HalfArcRotFullView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawHARFNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawHalfArcRotFull(scale, w, h, paint)
 }
+
+class HalfArcRotFullView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN ->{
+
+            }
+        }
+        return true
+    }
+}
