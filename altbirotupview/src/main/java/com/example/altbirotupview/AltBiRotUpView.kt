2a21fae087d5041db5bf792fafbd2aa3bb57d317
Anwesh43 <anweshthecool0@gmail.com>
2024-07-06 10:50:08 +0530

    altbirotupview: adding Canvas extension methods

--- altbirotupview/src/main/java/com/example/altbirotupview/AltBiRotUpView.kt
+++ altbirotupview/src/main/java/com/example/altbirotupview/AltBiRotUpView.kt
@@ -26,3 +26,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawAltBiRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(2)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                rotate(rot * j * dsc(1))
+                drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+                    drawRect(RectF(0f, -size, size, 0f), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawABRUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawAltBiRotUp(scale, w, h, paint)
+}
