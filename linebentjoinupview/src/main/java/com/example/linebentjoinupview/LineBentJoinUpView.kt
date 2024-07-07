37029c7aebc4b801127e801253fe2fb7e3ff3bf3
Anwesh43 <anweshthecool0@gmail.com>
2024-07-07 12:51:07 +0530

    lnebentjoinupview: adding Renderer in View

--- linebentjoinupview/src/main/java/com/example/linebentjoinupview/LineBentJoinUpView.kt
+++ linebentjoinupview/src/main/java/com/example/linebentjoinupview/LineBentJoinUpView.kt
@@ -59,14 +59,15 @@ fun Canvas.drawLBJUNode(i : Int, scale : Float, paint : Paint) {
 
 class LineBentJoinUpView(ctx : Context) : View(ctx) {
 
+    private val renderer : Renderer = Renderer(this)
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
