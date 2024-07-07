bcb16bffa80a93bed84bc42dc0b77263f60f518c
Anwesh43 <anweshthecool0@gmail.com>
2024-07-07 12:43:57 +0530

    lnebentjoinupview: adding LBJUNode class

--- linebentjoinupview/src/main/java/com/example/linebentjoinupview/LineBentJoinUpView.kt
+++ linebentjoinupview/src/main/java/com/example/linebentjoinupview/LineBentJoinUpView.kt
@@ -117,4 +117,45 @@ class LineBentJoinUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBJUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBJUNode? = null
+        private var prev : LBJUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBJUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBJUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBJUNode {
+            var curr : LBJUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
