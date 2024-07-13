cf8b9348edf92a63e38bb758bb7a4922dbcc6c8c
Anwesh43 <anweshthecool0@gmail.com>
2024-07-13 21:42:00 +0530

    linerotmoresqview: adding Renderer class

--- linerotmoresqview/src/main/java/com/example/linerotmoresqview/LineRotMoreSqView.kt
+++ linerotmoresqview/src/main/java/com/example/linerotmoresqview/LineRotMoreSqView.kt
@@ -188,4 +188,27 @@ class LineRotMoreSqView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotMoreSqView) {
+
+        private val animator : Animator = Animator(view)
+        private val lrms : LineRotMoreSq = LineRotMoreSq(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrms.draw(canvas, paint)
+            animator.animate {
+                lrms.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrms.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
