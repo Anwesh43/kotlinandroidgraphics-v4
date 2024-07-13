f5a1c729209ba31bbc836f6da01a96c22dfc671a
Anwesh43 <anweshthecool0@gmail.com>
2024-07-13 21:37:25 +0530

    linerotmoresqview: adding LRMSNode class

--- linerotmoresqview/src/main/java/com/example/linerotmoresqview/LineRotMoreSqView.kt
+++ linerotmoresqview/src/main/java/com/example/linerotmoresqview/LineRotMoreSqView.kt
@@ -124,4 +124,45 @@ class LineRotMoreSqView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRMSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRMSNode? = null
+        private var prev : LRMSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRMSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRMSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRMSNode {
+            var curr : LRMSNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
