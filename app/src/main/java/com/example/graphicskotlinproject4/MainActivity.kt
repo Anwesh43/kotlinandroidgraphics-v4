3910d7412de69bd94986b7cf13f3ca455a31af8a
Anwesh43 <anweshthecool0@gmail.com>
2024-07-13 21:44:11 +0530

    linerotmoresqview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.linebentjoinupview.LineBentJoinUpView
+import com.example.linerotmoresqview.LineRotMoreSqView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        LineBentJoinUpView.create(this)
+        LineRotMoreSqView.create(this)
     }
 }
