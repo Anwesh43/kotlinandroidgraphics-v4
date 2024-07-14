6023a2e026ae0f151d41b05d43dfb77ad19063d8
Anwesh43 <anweshthecool0@gmail.com>
2024-07-14 22:54:57 +0530

    bentlinerotview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.linerotmoresqview.LineRotMoreSqView
+import com.example.bentlinerotview.BentLineRotView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        LineRotMoreSqView.create(this)
+        BentLineRotView.create(this)
     }
 }
