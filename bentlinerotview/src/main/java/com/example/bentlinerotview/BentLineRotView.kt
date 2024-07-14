376c6f2b89df19646213d8cee859b0bf1fadb0a9
Anwesh43 <anweshthecool0@gmail.com>
2024-07-14 21:58:08 +0530

    bentlinerotview: adding BentLineRotView class

--- bentlinerotview/src/main/java/com/example/bentlinerotview/BentLineRotView.kt
+++ bentlinerotview/src/main/java/com/example/bentlinerotview/BentLineRotView.kt
@@ -58,3 +58,17 @@ fun Canvas.drawBLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBentLineRot(scale, w, h, paint)
 }
+
+class BentLineRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN
+        }
+        return true
+    }
+}
