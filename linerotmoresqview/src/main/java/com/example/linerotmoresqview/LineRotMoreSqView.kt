8165831160affa8b3b0878846be4d3de590c3370
Anwesh43 <anweshthecool0@gmail.com>
2024-07-14 20:40:30 +0530

    linerotmoresqview: adding fixes

--- linerotmoresqview/src/main/java/com/example/linerotmoresqview/LineRotMoreSqView.kt
+++ linerotmoresqview/src/main/java/com/example/linerotmoresqview/LineRotMoreSqView.kt
@@ -40,7 +40,7 @@ fun Canvas.drawLineRotMoreSq(scale : Float, w : Float, h : Float, paint : Paint)
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2, h / 2 - (h / 2 + size) * dsc(3)) {
         for (j in 0..1) {
             drawXY(-w * 0.5f * (1 - dsc(0)), 0f) {
                 rotate(rot * dsc(1) * j)
