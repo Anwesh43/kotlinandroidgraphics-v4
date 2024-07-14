e83bc97cc50a9f74fb843a522df4382b48c025a2
Anwesh43 <anweshthecool0@gmail.com>
2024-07-14 21:56:37 +0530

    bentlinerotview: adding Canvas extension methods

--- bentlinerotview/src/main/java/com/example/bentlinerotview/BentLineRotView.kt
+++ bentlinerotview/src/main/java/com/example/bentlinerotview/BentLineRotView.kt
@@ -21,8 +21,40 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 45f
+val rot : Float = 90f
+val deg : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBentLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        drawXY(size / 2, 0f) {
+            rotate(deg)
+            drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+        }
+    }
+}
+
+fun Canvas.drawBLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBentLineRot(scale, w, h, paint)
+}
