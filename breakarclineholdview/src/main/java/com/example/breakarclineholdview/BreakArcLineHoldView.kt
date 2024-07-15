8c2d659b8f7b854bbcb76f4a4a062b4bf772adff
Anwesh43 <anweshthecool0@gmail.com>
2024-07-15 11:45:42 +0530

    breakarclineholdview: adding BreakArcHoldLineView class

--- breakarclineholdview/src/main/java/com/example/breakarclineholdview/BreakArcLineHoldView.kt
+++ breakarclineholdview/src/main/java/com/example/breakarclineholdview/BreakArcLineHoldView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBALHNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBreakArcLineHold(scale, w, h, paint)
 }
+
+class BreakArcLineHoldView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
