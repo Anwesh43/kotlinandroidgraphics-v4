754785f35affffb288e3d311d5556eb371d28cb3
Anwesh43 <anweshthecool0@gmail.com>
2024-07-15 13:26:02 +0530

    breakarclineholdview: adding BreakArcLineHold class

--- breakarclineholdview/src/main/java/com/example/breakarclineholdview/BreakArcLineHoldView.kt
+++ breakarclineholdview/src/main/java/com/example/breakarclineholdview/BreakArcLineHoldView.kt
@@ -165,4 +165,27 @@ class BreakArcLineHoldView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BreakArcLineHold(var i : Int) {
+
+        private var curr : BALHNode = BALHNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
