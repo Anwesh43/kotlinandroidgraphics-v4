2fb2956b2d0c086ae14144f584ad12187cc8af8a
Anwesh43 <anweshthecool0@gmail.com>
2024-07-15 11:43:45 +0530

    breakarclineholdview: adding Canvas extension methods

--- breakarclineholdview/src/main/java/com/example/breakarclineholdview/BreakArcLineHoldView.kt
+++ breakarclineholdview/src/main/java/com/example/breakarclineholdview/BreakArcLineHoldView.kt
@@ -22,7 +22,42 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 180f
+val strokeFactor : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBreakArcLineHold(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(4)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(-rot * j * dsc(1))
+                drawArc(RectF(0f, -size / 2, size, size / 2), rot * j, rot * dsc(0).divideScale(j, 2), true, paint)
+            }
+            drawXY(0f, -h * 0.5f * (1 - dsc(2))) {
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBALHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBreakArcLineHold(scale, w, h, paint)
+}
