5631e4bcc9b8ff24297552919a0995ae2a44b547
Anwesh43 <anweshthecool0@gmail.com>
2024-07-16 10:18:40 +0530

    biarcompleteview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.breakarclineholdview.BreakArcLineHoldView
+import com.example.biarcompleteview.BiArcCompleteView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        BreakArcLineHoldView.create(this)
+        BiArcCompleteView.create(this)
     }
 }
