58f0c5ad3a8199ced83753f4d9f4dd00f9e6fd3e
Anwesh43 <anweshthecool0@gmail.com>
2024-07-16 09:59:43 +0530

    biarcompleteview: adding BiArcCompleteView class

--- biarcompleteview/src/main/java/com/example/biarcompleteview/BiArcCompleteView.kt
+++ biarcompleteview/src/main/java/com/example/biarcompleteview/BiArcCompleteView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBACNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiArcComplete(scale, w, h, paint)
 }
+
+class BiArcCompleteView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
