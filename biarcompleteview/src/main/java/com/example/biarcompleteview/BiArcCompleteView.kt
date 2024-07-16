e2c4a02317ae217a88331ef750ddc28524659581
Anwesh43 <anweshthecool0@gmail.com>
2024-07-16 10:17:18 +0530

    biarcompleteview: adding Renderer in View

--- biarcompleteview/src/main/java/com/example/biarcompleteview/BiArcCompleteView.kt
+++ biarcompleteview/src/main/java/com/example/biarcompleteview/BiArcCompleteView.kt
@@ -65,14 +65,16 @@ fun Canvas.drawBACNode(i : Int, scale : Float, paint : Paint) {
 
 class BiArcCompleteView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
