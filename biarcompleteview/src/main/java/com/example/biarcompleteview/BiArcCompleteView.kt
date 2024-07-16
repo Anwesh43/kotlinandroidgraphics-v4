953ba9fa5d29ac58c72f40737becaf4116ad5fe5
Anwesh43 <anweshthecool0@gmail.com>
2024-07-16 10:16:53 +0530

    biarcompleteview: adding Renderer class

--- biarcompleteview/src/main/java/com/example/biarcompleteview/BiArcCompleteView.kt
+++ biarcompleteview/src/main/java/com/example/biarcompleteview/BiArcCompleteView.kt
@@ -189,4 +189,27 @@ class BiArcCompleteView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiArcCompleteView) {
+
+        private val animator : Animator = Animator(view)
+        private val bac : BiArcComplete = BiArcComplete(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bac.draw(canvas, paint)
+            animator.animate {
+                bac.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bac.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
