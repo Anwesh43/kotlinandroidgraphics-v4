ebb023c51cad0b1e213b910480bc9c1b48400335
Anwesh43 <anweshthecool0@gmail.com>
2024-07-16 10:13:24 +0530

    biarcompleteview: adding BACNode class

--- biarcompleteview/src/main/java/com/example/biarcompleteview/BiArcCompleteView.kt
+++ biarcompleteview/src/main/java/com/example/biarcompleteview/BiArcCompleteView.kt
@@ -125,4 +125,45 @@ class BiArcCompleteView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BACNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BACNode? = null
+        private var prev : BACNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BACNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBACNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BACNode {
+            var curr : BACNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
