e7b67c198e4a3a294a40f5e8da2875f7b0cd7492
Anwesh43 <anweshthecool0@gmail.com>
2024-07-18 10:22:35 +0530

    linetoarccompleteview: adding LineToArcCompleteView class

--- linetoarccompleteview/src/main/java/com/example/linetoarccompleteview/LineToArcCompleteView.kt
+++ linetoarccompleteview/src/main/java/com/example/linetoarccompleteview/LineToArcCompleteView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLTACNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineToArcComplete(scale, w, h, paint)
 }
+
+class LineToArcCompleteView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
