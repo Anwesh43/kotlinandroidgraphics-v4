fbc028556c7c7602ebc71748b39c753827f49859
Anwesh43 <anweshthecool0@gmail.com>
2024-07-18 10:34:06 +0530

    linetoarccompleteview: adding LineToArcComplete class

--- linetoarccompleteview/src/main/java/com/example/linetoarccompleteview/LineToArcCompleteView.kt
+++ linetoarccompleteview/src/main/java/com/example/linetoarccompleteview/LineToArcCompleteView.kt
@@ -167,4 +167,27 @@ class LineToArcCompleteView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineToArcComplete(var i : Int) {
+
+        private var curr : LTACNode = LTACNode(0)
+        private var dir : Int =1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
