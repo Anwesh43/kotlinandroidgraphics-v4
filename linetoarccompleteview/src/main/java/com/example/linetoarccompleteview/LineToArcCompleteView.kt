66401d46e9f86609b34fcaa7e7ba65c92da3f5f1
Anwesh43 <anweshthecool0@gmail.com>
2024-07-18 10:32:51 +0530

    linetoarccompleteview: adding LTACNode class

--- linetoarccompleteview/src/main/java/com/example/linetoarccompleteview/LineToArcCompleteView.kt
+++ linetoarccompleteview/src/main/java/com/example/linetoarccompleteview/LineToArcCompleteView.kt
@@ -126,4 +126,45 @@ class LineToArcCompleteView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTACNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LTACNode? = null
+        private var prev : LTACNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTACNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTACNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTACNode {
+            var curr : LTACNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
