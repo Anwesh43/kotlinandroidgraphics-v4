ed40def0c4f2978038cf1e2df3dc25ed3a8b99ec
Anwesh43 <anweshthecool0@gmail.com>
2024-07-19 11:10:06 +0530

    linerotintoarcview: adding Renderer class

--- linerotintoarcview/src/main/java/com/example/linerotintoarcview/LineRotIntoArcView.kt
+++ linerotintoarcview/src/main/java/com/example/linerotintoarcview/LineRotIntoArcView.kt
@@ -187,4 +187,27 @@ class LineRotIntoArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotIntoArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val lria : LineRotIntoArc = LineRotIntoArc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lria.draw(canvas, paint)
+            animator.animate {
+                lria.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lria.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
