2c3f31af4ce29bfac4088fec4ca6e65dbc3f45fd
Anwesh43 <anweshthecool0@gmail.com>
2024-07-19 10:51:56 +0530

    linerotintoarcview: adding LRIANode class

--- linerotintoarcview/src/main/java/com/example/linerotintoarcview/LineRotIntoArcView.kt
+++ linerotintoarcview/src/main/java/com/example/linerotintoarcview/LineRotIntoArcView.kt
@@ -123,4 +123,45 @@ class LineRotIntoArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRIANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRIANode? = null
+        private var prev : LRIANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRIANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRIANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRIANode {
+            var curr : LRIANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
