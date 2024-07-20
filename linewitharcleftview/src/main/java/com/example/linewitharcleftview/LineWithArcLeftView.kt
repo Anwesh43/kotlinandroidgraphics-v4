8474210e15feccf97110ec2891046341af570ac3
Anwesh43 <anweshthecool0@gmail.com>
2024-07-20 10:05:08 +0530

    linewitharcleftview: adding LineWithArcLeftView class

--- linewitharcleftview/src/main/java/com/example/linewitharcleftview/LineWithArcLeftView.kt
+++ linewitharcleftview/src/main/java/com/example/linewitharcleftview/LineWithArcLeftView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawLWALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineWithArcLeft(scale, w, h, paint)
 }
+
+class LineWithArcLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
