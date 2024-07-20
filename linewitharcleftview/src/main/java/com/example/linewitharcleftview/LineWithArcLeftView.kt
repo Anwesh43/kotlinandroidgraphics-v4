e3ea69163467fc62de2f9506fe0c47e4cdae4b71
Anwesh43 <anweshthecool0@gmail.com>
2024-07-20 10:03:53 +0530

    linewitharcleftview: adding Canvas extension methods

--- linewitharcleftview/src/main/java/com/example/linewitharcleftview/LineWithArcLeftView.kt
+++ linewitharcleftview/src/main/java/com/example/linewitharcleftview/LineWithArcLeftView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,31 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineWithArcLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        drawArc(RectF(-size / 2, 0f, size / 2, size), 90f, 180f * (dsc(1) + dsc(3)), false, paint)
+    }
+}
+
+fun Canvas.drawLWALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineWithArcLeft(scale, w, h, paint)
+}
