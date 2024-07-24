ca1226b5fb2a668c3f1da2645c8b16c0b843f81c
Anwesh43 <anweshthecool0@gmail.com>
2024-07-24 20:07:14 +0530

    sqrotfromarcview: adding SFRANode class

--- sqrotfromarcview/src/main/java/com/example/sqrotfromarcview/SqRotFromArcView.kt
+++ sqrotfromarcview/src/main/java/com/example/sqrotfromarcview/SqRotFromArcView.kt
@@ -122,4 +122,45 @@ class SqRotFromArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SRFANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SRFANode? = null
+        private var prev : SRFANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SRFANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSRFANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SRFANode {
+            var curr : SRFANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
