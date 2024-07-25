1e08824f0cd8c8880a24b799361dcadf325d255b
Anwesh43 <anweshthecool0@gmail.com>
2024-07-25 20:25:04 +0530

    linerotangleleftview: adding Canvas extension methods

--- linerotangleleftview/src/main/java/com/example/linerotangleleftview/LineRotAngleLeftView.kt
+++ linerotangleleftview/src/main/java/com/example/linerotangleleftview/LineRotAngleLeftView.kt
@@ -3,7 +3,7 @@ package com.example.linerotangleleftview
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.Canvas
 import android.graphics.Color
 import android.app.Activity
 import android.content.Context
@@ -15,7 +15,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -26,3 +26,36 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotAngleLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(-rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        }
+        drawXY(-size, -h * 0.5f * (1 - dsc(2))) {
+            rotate(rot * dsc(3))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+    }
+}
+
+fun Canvas.drawLRALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotAngleLeft(scale, w, h, paint)
+}
