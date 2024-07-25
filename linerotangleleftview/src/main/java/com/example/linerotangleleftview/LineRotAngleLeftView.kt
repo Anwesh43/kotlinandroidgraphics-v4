ffc95e4eef161d29e67f295087b08aafbbe22b14
Anwesh43 <anweshthecool0@gmail.com>
2024-07-25 20:29:30 +0530

    linerotangleleftview: adding LineRotAngleLeftView class

--- linerotangleleftview/src/main/java/com/example/linerotangleleftview/LineRotAngleLeftView.kt
+++ linerotangleleftview/src/main/java/com/example/linerotangleleftview/LineRotAngleLeftView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLRALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotAngleLeft(scale, w, h, paint)
 }
+
+class LineRotAngleLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvs : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
