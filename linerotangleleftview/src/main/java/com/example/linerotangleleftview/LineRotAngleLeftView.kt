a223d50d99e2713523316399fb0c52351382b035
Anwesh43 <anweshthecool0@gmail.com>
2024-07-26 10:02:24 +0530

    linerotangleleftview: adding Renderer in View

--- linerotangleleftview/src/main/java/com/example/linerotangleleftview/LineRotAngleLeftView.kt
+++ linerotangleleftview/src/main/java/com/example/linerotangleleftview/LineRotAngleLeftView.kt
@@ -62,14 +62,16 @@ fun Canvas.drawLRALNode(i : Int, scale : Float, paint : Paint) {
 
 class LineRotAngleLeftView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvs : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
