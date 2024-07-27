f87017d8d19d46da14e20b6fc5ad77d35f70c541
Anwesh43 <anweshthecool0@gmail.com>
2024-07-27 16:14:21 +0530

    linesqrotholderview: adding LRSHNode class

--- linesqrotholderview/src/main/java/com/example/linesqrotholderview/LineRotSqHolderView.kt
+++ linesqrotholderview/src/main/java/com/example/linesqrotholderview/LineRotSqHolderView.kt
@@ -123,4 +123,45 @@ class LineRotSqHolderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRSHNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRSHNode? = null
+        private var prev : LRSHNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRSHNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRSHNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRSHNode {
+            var curr : LRSHNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
