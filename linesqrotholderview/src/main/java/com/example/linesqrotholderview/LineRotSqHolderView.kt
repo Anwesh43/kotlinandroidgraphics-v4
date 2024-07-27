25d6d52fe30533b6ad92411763ce90c26a393f7d
Anwesh43 <anweshthecool0@gmail.com>
2024-07-27 14:00:45 +0530

    linesqrotholderview: adding LineRotSqHolderView class

--- linesqrotholderview/src/main/java/com/example/linesqrotholderview/LineRotSqHolderView.kt
+++ linesqrotholderview/src/main/java/com/example/linesqrotholderview/LineRotSqHolderView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLRSHNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / sizeFactor
     drawLineRotSqHolder(scale, w , h, paint)
 }
+
+class LineRotSqHolderView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
