d832093951c05bea5bd6c8b94743d68339e7e38d
Anwesh43 <anweshthecool0@gmail.com>
2024-07-29 02:17:54 +0530

    linehalfarcrotleftview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.linehalfarcbreakview.LineHalfArcBreakView
+import com.example.linehalfarcrotleftview.LineHalfArcRotLeftView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        LineHalfArcBreakView.create(this)
+        LineHalfArcRotLeftView.create(this)
 
     }
 }
