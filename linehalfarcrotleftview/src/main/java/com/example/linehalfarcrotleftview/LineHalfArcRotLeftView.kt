a306ffd9e369dc58d6c269db09a338b4561ff45e
Anwesh43 <anweshthecool0@gmail.com>
2024-07-29 02:07:31 +0530

    linehalfarcrotleftview: adding LineHalfArcRotLeftView class

--- linehalfarcrotleftview/src/main/java/com/example/linehalfarcrotleftview/LineHalfArcRotLeftView.kt
+++ linehalfarcrotleftview/src/main/java/com/example/linehalfarcrotleftview/LineHalfArcRotLeftView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLHARLNode(i: Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineHalfArcRotLeft(scale, w, h, paint)
 }
+
+class LineHalfArcRotLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
