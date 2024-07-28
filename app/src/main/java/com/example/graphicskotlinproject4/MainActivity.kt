30086eb83f35d877a3603d6467c3e764e07b05fc
Anwesh43 <anweshthecool0@gmail.com>
2024-07-28 14:11:52 +0530

    linehalfarcbreakview: adding view in acitivty

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.linesqrotholderview.LineRotSqHolderView
+import com.example.linehalfarcbreakview.LineHalfArcBreakView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        LineRotSqHolderView.create(this)
+        LineHalfArcBreakView.create(this)
 
     }
 }
