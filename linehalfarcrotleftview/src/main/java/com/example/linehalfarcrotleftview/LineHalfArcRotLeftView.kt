d8a82250abac9e911105ddb709c4258735f18b4e
Anwesh43 <anweshthecool0@gmail.com>
2024-07-29 02:12:54 +0530

    linehalfarcrotleftview: adding LHARLNode class

--- linehalfarcrotleftview/src/main/java/com/example/linehalfarcrotleftview/LineHalfArcRotLeftView.kt
+++ linehalfarcrotleftview/src/main/java/com/example/linehalfarcrotleftview/LineHalfArcRotLeftView.kt
@@ -120,4 +120,45 @@ class LineHalfArcRotLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LHARLNode(var i : Int  = 0, val state : State = State()) {
+
+        private var next : LHARLNode? = null
+        private var prev : LHARLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LHARLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLHARLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LHARLNode {
+            var curr : LHARLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
