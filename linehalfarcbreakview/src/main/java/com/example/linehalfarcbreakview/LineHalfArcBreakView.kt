49acd5ea1b1b05875d038377ef12fd3abdb97a80
Anwesh43 <anweshthecool0@gmail.com>
2024-07-28 13:31:37 +0530

    linehalfarcbreakview: adding Animator class

--- linehalfarcbreakview/src/main/java/com/example/linehalfarcbreakview/LineHalfArcBreakView.kt
+++ linehalfarcbreakview/src/main/java/com/example/linehalfarcbreakview/LineHalfArcBreakView.kt
@@ -91,4 +91,32 @@ class LineHalfArcBreakView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun sop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
