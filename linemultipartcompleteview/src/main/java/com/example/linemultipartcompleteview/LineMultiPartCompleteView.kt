9b2fbc1ede6cbe941edaaf38d6eee6808aa61bea
Anwesh43 <anweshthecool0@gmail.com>
2024-08-02 00:57:11 +0530

    linemultipartcompleteview: adding LineMultiPartCompleteView class

--- linemultipartcompleteview/src/main/java/com/example/linemultipartcompleteview/LineMultiPartCompleteView.kt
+++ linemultipartcompleteview/src/main/java/com/example/linemultipartcompleteview/LineMultiPartCompleteView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLMPCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineMultiPartComplete(i, scale, w, h, paint)
 }
+
+class LineMultiPartCompleteView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
