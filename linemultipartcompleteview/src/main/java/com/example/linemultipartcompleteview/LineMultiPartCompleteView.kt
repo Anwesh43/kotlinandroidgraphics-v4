5bf929a942a9914ad4ddc28f273b204445d667d2
Anwesh43 <anweshthecool0@gmail.com>
2024-08-02 00:56:03 +0530

    linemultipartcompleteview: adding Canvas extension methods

--- linemultipartcompleteview/src/main/java/com/example/linemultipartcompleteview/LineMultiPartCompleteView.kt
+++ linemultipartcompleteview/src/main/java/com/example/linemultipartcompleteview/LineMultiPartCompleteView.kt
@@ -3,8 +3,9 @@ package com.example.linemultipartcompleteview
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.RectF
 import android.app.Activity
 import android.content.Context
 
@@ -26,3 +27,34 @@ val rot : Float = 360f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineMultiPartComplete(i : Int, scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val startDeg : Float = (rot / colors.size) * i
+    drawXY(w / 2, h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1) + startDeg)
+            drawLine(0f, 0f, 0f, -size * (dsc(0) - dsc(2)), paint)
+        }
+        drawArc(RectF(-size, -size, size, size), startDeg + rot * dsc(2), rot * (dsc(1) - dsc(2)), true, paint)
+    }
+}
+
+fun Canvas.drawLMPCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineMultiPartComplete(i, scale, w, h, paint)
+}
