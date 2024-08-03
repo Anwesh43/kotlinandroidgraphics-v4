23658f4428eebace01e0a67588f343d8e7678cdb
Anwesh43 <anweshthecool0@gmail.com>
2024-08-04 00:37:33 +0530

    arclinecompletejoinview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.linemultipartcompleteview.LineMultiPartCompleteView
+import com.example.arclinecompletejoinview.ArcLineCompleteJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        LineMultiPartCompleteView.create(this)
+        ArcLineCompleteJoinView.create(this)
 
     }
 }
