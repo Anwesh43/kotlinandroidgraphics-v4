717ca3cbe6c5765ffa0fa9b9a0daba4cb12529d2
Anwesh43 <anweshthecool0@gmail.com>
2024-08-03 22:28:38 +0530

    arclinecompletejoinview: adding ArcLineCompleteJoinView class

--- arclinecompletejoinview/src/main/java/com/example/arclinecompletejoinview/ArcLineCompleteJoinView.kt
+++ arclinecompletejoinview/src/main/java/com/example/arclinecompletejoinview/ArcLineCompleteJoinView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawALCJNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcLineCompleteJoin(scale, w, h, paint)
 }
+
+class ArcLineCompleteJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas: Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
