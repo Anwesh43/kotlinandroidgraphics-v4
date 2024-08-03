60d64739acf2e0fdc63ed2a9f74818e3f12b4ed8
Anwesh43 <anweshthecool0@gmail.com>
2024-08-03 23:59:07 +0530

    arclinecompletejoinview: adding ALJCNode class

--- arclinecompletejoinview/src/main/java/com/example/arclinecompletejoinview/ArcLineCompleteJoinView.kt
+++ arclinecompletejoinview/src/main/java/com/example/arclinecompletejoinview/ArcLineCompleteJoinView.kt
@@ -124,4 +124,45 @@ class ArcLineCompleteJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALCJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALCJNode? = null
+        private var prev : ALCJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALCJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALCJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALCJNode {
+            var curr : ALCJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
