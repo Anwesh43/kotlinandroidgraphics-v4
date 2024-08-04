e3ff6070d27fb1fdfeeea874dc7780589f8e292b
Anwesh43 <anweshthecool0@gmail.com>
2024-08-04 12:24:35 +0530

    arcleftlinecompleteview: adding ArcLeftLineCompleteView class

--- arcleftlinecompleteview/src/main/java/com/example/arcleftlinecompleteview/ArcLeftLineCompleteView.kt
+++ arcleftlinecompleteview/src/main/java/com/example/arcleftlinecompleteview/ArcLeftLineCompleteView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawALLCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcLeftLineComplete(scale, w, h, paint)
 }
+
+class ArcLeftLineCompleteView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
