bb54164876fa999f30fcce9a73ef7648cf2d8bf4
Anwesh43 <anweshthecool0@gmail.com>
2024-08-04 12:19:41 +0530

    arcleftlinecompleteview: adding Canvas extension methods

--- arcleftlinecompleteview/src/main/java/com/example/arcleftlinecompleteview/ArcLeftLineCompleteView.kt
+++ arcleftlinecompleteview/src/main/java/com/example/arcleftlinecompleteview/ArcLeftLineCompleteView.kt
@@ -27,3 +27,30 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcLeftLineComplete(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawLine(0f, size * (1 - dsc(0)), 0f, size * (1 - dsc(1)), paint)
+        drawArc(RectF(-size, -size / 2, 0f, size / 2), rot * dsc(3), rot * (dsc(2) - dsc(3)), false, paint)
+    }
+}
+
+fun Canvas.drawALLCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcLeftLineComplete(scale, w, h, paint)
+}
