d8d144e1236cbcd8a67587cd481c672f0a0963fa
Anwesh43 <anweshthecool0@gmail.com>
2024-08-05 14:34:39 +0530

    altmoverightlineview: adding AMRLNode class

--- altmoverightlineview/src/main/java/com/example/altmoverightlineview/AltMoveRightLineView.kt
+++ altmoverightlineview/src/main/java/com/example/altmoverightlineview/AltMoveRightLineView.kt
@@ -121,4 +121,45 @@ class AltMoveRightLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AMRLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AMRLNode? = null
+        private var prev : AMRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AMRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAMRLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AMRLNode {
+            var curr : AMRLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
