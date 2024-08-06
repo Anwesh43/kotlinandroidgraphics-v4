627c669768d50986574509600a345fdeb6f11e1a
Anwesh43 <anweshthecool0@gmail.com>
2024-08-06 08:43:39 +0530

    linearcrightexendview: adding LARENode class

--- linearcrightextendview/src/main/java/com/example/linearcrightextendview/LineArcRightExtendView.kt
+++ linearcrightextendview/src/main/java/com/example/linearcrightextendview/LineArcRightExtendView.kt
@@ -121,4 +121,45 @@ class LineArcRightExtendView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LARENode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LARENode? = null
+        private var prev : LARENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LARENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLARENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LARENode {
+            var curr : LARENode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
