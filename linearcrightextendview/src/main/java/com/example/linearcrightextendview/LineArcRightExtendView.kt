ae823edd3e2b5c347e086d89598064d73009d9ac
Anwesh43 <anweshthecool0@gmail.com>
2024-08-06 08:38:38 +0530

    linearcrightexendview: adding LineArcRightExtendView class

--- linearcrightextendview/src/main/java/com/example/linearcrightextendview/LineArcRightExtendView.kt
+++ linearcrightextendview/src/main/java/com/example/linearcrightextendview/LineArcRightExtendView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLARENode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineArcRightExtend(scale, w, h, paint)
 }
+
+class LineArcRightExtendView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
