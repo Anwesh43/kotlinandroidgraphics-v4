28773a86551d49bea880b8b17830ee49ac83f93a
Anwesh43 <anweshthecool0@gmail.com>
2024-08-06 08:49:19 +0530

    linearcrightexendview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.altmoverightlineview.AltMoveRightLineView
+import com.example.linearcrightextendview.LineArcRightExtendView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        AltMoveRightLineView.create(this)
+        LineArcRightExtendView.create(this)
     }
 }
