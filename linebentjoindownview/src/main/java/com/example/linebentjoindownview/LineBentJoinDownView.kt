4066c2bb695417f7acb5267fd228b450bd2562e3
Anwesh43 <anweshthecool0@gmail.com>
2024-08-07 20:16:21 +0530

    linebentjoindownview : adding LBJDNode class

--- linebentjoindownview/src/main/java/com/example/linebentjoindownview/LineBentJoinDownView.kt
+++ linebentjoindownview/src/main/java/com/example/linebentjoindownview/LineBentJoinDownView.kt
@@ -123,4 +123,45 @@ class LineBentJoinDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBJDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBJDNode? = null
+        private var prev : LBJDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBJDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas: Canvas, paint : Paint) {
+            canvas.drawLBJDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBJDNode {
+            var curr : LBJDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
