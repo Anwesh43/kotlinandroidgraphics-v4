a7c374bc6fab3ff575bdb00975c5d76fcc9601a7
Anwesh43 <anweshthecool0@gmail.com>
2024-08-07 20:08:30 +0530

    linebentjoindownview : adding Canvas extension methods

--- linebentjoindownview/src/main/java/com/example/linebentjoindownview/LineBentJoinDownView.kt
+++ linebentjoindownview/src/main/java/com/example/linebentjoindownview/LineBentJoinDownView.kt
@@ -15,8 +15,8 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
@@ -27,3 +27,36 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentJoinDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(rot * dsc(4))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(deg * (1 - dsc(2 + j)))
+                drawLine(0f, 0f, 0f, -size * dsc(j), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBJDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBentJoinDown(scale, w, h, paint)
+}
