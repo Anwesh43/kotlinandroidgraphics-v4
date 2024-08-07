4d76d4e28fdf720f9998fb6ac2ae6b8da3adecb9
Anwesh43 <anweshthecool0@gmail.com>
2024-08-07 20:22:57 +0530

    linebentjoindownview : adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.linearcrightextendview.LineArcRightExtendView
+import com.example.linebentjoindownview.LineBentJoinDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        LineArcRightExtendView.create(this)
+        LineBentJoinDownView.create(this)
     }
 }
