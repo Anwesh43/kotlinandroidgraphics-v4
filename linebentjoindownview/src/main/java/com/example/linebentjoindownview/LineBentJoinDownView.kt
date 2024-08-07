7c1ccd0a2162cbb9bdeac8b5580948ab9508b474
Anwesh43 <anweshthecool0@gmail.com>
2024-08-07 20:09:20 +0530

    linebentjoindownview : adding LineBentJoinDownView class

--- linebentjoindownview/src/main/java/com/example/linebentjoindownview/LineBentJoinDownView.kt
+++ linebentjoindownview/src/main/java/com/example/linebentjoindownview/LineBentJoinDownView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLBJDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentJoinDown(scale, w, h, paint)
 }
+
+class LineBentJoinDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
