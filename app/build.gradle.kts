bd742991ced77b8cbf48e3254f8b7d516a729158
Anwesh43 <anweshthecool0@gmail.com>
2024-08-08 14:39:56 +0530

    startcompleterotarcview: adding view in activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(project(":linebentjoindownview"))
+    implementation(project(":startcompleterotarcview"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.linebentjoindownview.LineBentJoinDownView
+import com.example.startcompleterotarcview.StartCompleteArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        LineBentJoinDownView.create(this)
+        StartCompleteArcView.create(this)
     }
 }
