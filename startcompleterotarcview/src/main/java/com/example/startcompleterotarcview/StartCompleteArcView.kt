e256f91c7ce5d11650788c42e087f86d21b7d7e9
Anwesh43 <anweshthecool0@gmail.com>
2024-08-08 14:33:21 +0530

    startcompleterotarcview: adding SCANode class

--- startcompleterotarcview/src/main/java/com/example/startcompleterotarcview/StartCompleteArcView.kt
+++ startcompleterotarcview/src/main/java/com/example/startcompleterotarcview/StartCompleteArcView.kt
@@ -123,4 +123,45 @@ class StartCompleteArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SCANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SCANode? = null
+        private var prev : SCANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SCANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSCANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SCANode {
+            var curr : SCANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
