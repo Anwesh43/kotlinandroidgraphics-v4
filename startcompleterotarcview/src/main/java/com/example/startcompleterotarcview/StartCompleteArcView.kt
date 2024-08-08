0bbcb1781272f07aadd3102f3c0379b166ba9235
Anwesh43 <anweshthecool0@gmail.com>
2024-08-08 13:36:15 +0530

    startcompleterotarcview: adding module setup

--- startcompleterotarcview/src/main/java/com/example/startcompleterotarcview/StartCompleteArcView.kt
+++ startcompleterotarcview/src/main/java/com/example/startcompleterotarcview/StartCompleteArcView.kt
@@ -27,3 +27,36 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawStartCompleteArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, -size + size * j) {
+                val updatingDeg = 180f * (1 - j) * dsc(j + 1)
+                drawArc(RectF(0f, 0f, size, size), -90f + updatingDeg, 180f * dsc(0) - updatingDeg, false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSCANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawStartCompleteArc(scale, w, h, paint)
+}
