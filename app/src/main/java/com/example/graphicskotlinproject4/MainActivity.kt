53f8d7bd8920a4cabe5c75569bcb0e46071fa6dc
Anwesh43 <anweshthecool0@gmail.com>
2024-08-09 15:02:28 +0530

    rotwitharclineview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.startcompleterotarcview.StartCompleteArcView
+import com.example.rotwitharclineview.RotWithArcLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        StartCompleteArcView.create(this)
+        RotWithArcLineView.create(this)
     }
 }
