d185c49b0a98146056aab6b4e74f31558fd2596b
Anwesh43 <anweshthecool0@gmail.com>
2024-08-09 14:43:16 +0530

    rotwitharclineview: adding RWALNode class

--- rotwitharclineview/src/main/java/com/example/rotwitharclineview/RotWithArcLineView.kt
+++ rotwitharclineview/src/main/java/com/example/rotwitharclineview/RotWithArcLineView.kt
@@ -120,4 +120,45 @@ class RotWithArcLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RWALNode(var i : Int = 0, private val state : State = State()) {
+
+        private var next : RWALNode? = null
+        private var prev : RWALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RWALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRWALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RWALNode {
+            var curr : RWALNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
