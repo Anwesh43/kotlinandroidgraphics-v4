a8c214b9d39a798c4d497ebacef1c88c54767c90
Anwesh43 <anweshthecool0@gmail.com>
2024-08-10 12:56:55 +0530

    rotarcquarterlineview: adding Int and Float extension methods

--- rotarcquarterlineview/src/main/java/com/example/rotarcquarterlineview/RotArcQuarterLineView.kt
+++ rotarcquarterlineview/src/main/java/com/example/rotarcquarterlineview/RotArcQuarterLineView.kt
@@ -23,3 +23,7 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
 val sizeFactor : Float = 4.9f
 val strokeFactor : Float = 90f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = 1f / this
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
