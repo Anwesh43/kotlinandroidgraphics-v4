8ccfe4bc2b837a182fd3988619bca49335e8bfe1
Anwesh43 <anweshthecool0@gmail.com>
2024-08-10 20:17:27 +0530

    rotarcquarterlineview: adding RotArcQuarterLineView class

--- rotarcquarterlineview/src/main/java/com/example/rotarcquarterlineview/RotArcQuarterLineView.kt
+++ rotarcquarterlineview/src/main/java/com/example/rotarcquarterlineview/RotArcQuarterLineView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawRAQLNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawRotArcQuarterLine(scale, w, h, paint)
 }
+
+class RotArcQuarterLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
