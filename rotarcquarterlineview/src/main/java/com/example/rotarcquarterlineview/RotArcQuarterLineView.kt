44bc3a713313570c21d73862e57b7dc70b980434
Anwesh43 <anweshthecool0@gmail.com>
2024-08-10 13:06:57 +0530

    rotarcquarterlineview: adding Canvas extension methods

--- rotarcquarterlineview/src/main/java/com/example/rotarcquarterlineview/RotArcQuarterLineView.kt
+++ rotarcquarterlineview/src/main/java/com/example/rotarcquarterlineview/RotArcQuarterLineView.kt
@@ -27,3 +27,36 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = 1f / this
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRotArcQuarterLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * (1 - dsc(2)))
+                drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), 90f + rot * dsc(2), 180f * (dsc(0) - dsc(2)), false, paint)
+    }
+}
+
+fun Canvas.drawRAQLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawRotArcQuarterLine(scale, w, h, paint)
+}
