b831cf97a474f31ed296374b78d4815a62f6d8bf
Anwesh43 <anweshthecool0@gmail.com>
2024-08-11 12:54:36 +0530

    linerotconcarcview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.rotarcquarterlineview.RotArcQuarterLineView
+import com.example.linerotconcarcview.LineRotConcArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        RotArcQuarterLineView.create(this)
+        LineRotConcArcView.create(this)
     }
 }
