493f16a034cbf07562fe1bd66d8d84435cbb2904
Anwesh43 <anweshthecool0@gmail.com>
2024-08-11 12:24:57 +0530

    linerotconcarcview: adding LRCANode class

--- linerotconcarcview/src/main/java/com/example/linerotconcarcview/LineRotConcArcView.kt
+++ linerotconcarcview/src/main/java/com/example/linerotconcarcview/LineRotConcArcView.kt
@@ -124,4 +124,45 @@ class LineRotConcArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRCANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRCANode? = null
+        private var prev ; LRCANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRCANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRCANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRCANode {
+            var curr : LRCANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
