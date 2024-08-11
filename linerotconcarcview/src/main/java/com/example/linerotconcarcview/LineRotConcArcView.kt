6848540be40ee78bdc3286783fdbf88fa3c512ed
Anwesh43 <anweshthecool0@gmail.com>
2024-08-11 11:43:15 +0530

    linerotconcarcview: adding LineRotConcArcView class

--- linerotconcarcview/src/main/java/com/example/linerotconcarcview/LineRotConcArcView.kt
+++ linerotconcarcview/src/main/java/com/example/linerotconcarcview/LineRotConcArcView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLRCANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineRotConcArc(scale, w, h, paint)
 }
+
+class LineRotConcArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
