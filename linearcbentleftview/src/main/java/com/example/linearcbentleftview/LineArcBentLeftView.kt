a0f3c65d68e7045067ec21363ad5c85e92e6db9b
Anwesh43 <anweshthecool0@gmail.com>
2024-08-13 03:12:02 +0530

    linearcbentleftview: adding LABLNode class

--- linearcbentleftview/src/main/java/com/example/linearcbentleftview/LineArcBentLeftView.kt
+++ linearcbentleftview/src/main/java/com/example/linearcbentleftview/LineArcBentLeftView.kt
@@ -123,4 +123,45 @@ class LineArcBentLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LABLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LABLNode? = null
+        private var prev : LABLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LABLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLABLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LABLNode {
+            var curr : LABLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
