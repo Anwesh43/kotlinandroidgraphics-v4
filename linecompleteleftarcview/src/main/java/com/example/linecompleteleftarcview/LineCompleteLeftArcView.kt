1c6ab65ed11ee6e51dff1d665f6721f4368e17d2
Anwesh43 <anweshthecool0@gmail.com>
2024-08-15 00:11:25 +0530

    linecompleteleftarcview: add LineCompleteLeftArcView class

--- linecompleteleftarcview/src/main/java/com/example/linecompleteleftarcview/LineCompleteLeftArcView.kt
+++ linecompleteleftarcview/src/main/java/com/example/linecompleteleftarcview/LineCompleteLeftArcView.kt
@@ -56,3 +56,19 @@ fun Canvas.drrawLCLANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLineCompleteLeftArc(scale, w, h, paint)
 }
+
+class LineCompleteLeftArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
