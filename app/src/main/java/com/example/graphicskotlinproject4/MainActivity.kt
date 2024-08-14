7152eb71c05862f037c0a54fe9c6d2e3fb1c031e
Anwesh43 <anweshthecool0@gmail.com>
2024-08-15 00:41:03 +0530

    linecompleteleftarcview: add view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.linearcbentleftview.LineArcBentLeftView
+import com.example.linecompleteleftarcview.LineCompleteLeftArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        LineArcBentLeftView.create(this)
+        LineCompleteLeftArcView.create(this)
     }
 }
