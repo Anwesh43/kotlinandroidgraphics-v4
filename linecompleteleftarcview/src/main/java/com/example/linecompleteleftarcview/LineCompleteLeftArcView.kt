0e695672eeffac1bb48d2864769238b3a6bd4332
Anwesh43 <anweshthecool0@gmail.com>
2024-08-15 00:34:36 +0530

    linecompleteleftarcview: add LineCompleteLeftArc class

--- linecompleteleftarcview/src/main/java/com/example/linecompleteleftarcview/LineCompleteLeftArcView.kt
+++ linecompleteleftarcview/src/main/java/com/example/linecompleteleftarcview/LineCompleteLeftArcView.kt
@@ -160,4 +160,28 @@ class LineCompleteLeftArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineCompleteLeftArc(var i : Int) {
+
+        private var curr : LCLANode = LCLANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
+
 }
