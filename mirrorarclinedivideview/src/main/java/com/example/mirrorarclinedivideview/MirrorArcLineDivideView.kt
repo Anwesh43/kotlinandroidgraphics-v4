568cb464fd1093de09c2b5f1441d05d82e228ba5
Anwesh43 <anweshthecool0@gmail.com>
2024-08-15 21:19:59 +0530

    mirrorarclinedivideview: adding MALDNode class

--- mirrorarclinedivideview/src/main/java/com/example/mirrorarclinedivideview/MirrorArcLineDivideView.kt
+++ mirrorarclinedivideview/src/main/java/com/example/mirrorarclinedivideview/MirrorArcLineDivideView.kt
@@ -123,4 +123,45 @@ class MirrorArcLineDivideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MALDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : MALDNode? = null
+        private var prev : MALDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MALDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMALDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MALDNode {
+            var curr : MALDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
