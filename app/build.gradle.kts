8187d4fc422cd7d986cc4830bd93161167975646
Anwesh43 <anweshthecool0@gmail.com>
2024-08-15 21:34:04 +0530

    mirrorarclinedivideview: adding view in activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(project(":linecompleteleftarcview"))
+    implementation(project(":mirrorarclinedivideview"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.linecompleteleftarcview.LineCompleteLeftArcView
+import com.example.mirrorarclinedivideview.MirrorArcLineDivideView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        LineCompleteLeftArcView.create(this)
+        MirrorArcLineDivideView.create(this)
     }
 }
