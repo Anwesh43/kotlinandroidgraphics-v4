08bf1075d47dcddb7ebe8b4a20a41dabcc65d286
Anwesh43 <anweshthecool0@gmail.com>
2024-08-15 21:34:47 +0530

    mirrorarclinedivideview: adding fix

--- mirrorarclinedivideview/src/main/java/com/example/mirrorarclinedivideview/MirrorArcLineDivideView.kt
+++ mirrorarclinedivideview/src/main/java/com/example/mirrorarclinedivideview/MirrorArcLineDivideView.kt
@@ -58,6 +58,7 @@ fun Canvas.drawMALDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawMirrorArcLineDivide(scale, w, h, paint)
 }
 
