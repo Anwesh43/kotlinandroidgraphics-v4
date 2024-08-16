b7b03f43fe8f7a87b997262e52c2c30acf414c34
Anwesh43 <anweshthecool0@gmail.com>
2024-08-16 16:15:36 +0530

    linearcrotfillview: adding Canvas extension methods

--- linearcrotfillview/src/main/java/com/example/linearcrotfillview/LineArcRotFillView.kt
+++ linearcrotfillview/src/main/java/com/example/linearcrotfillview/LineArcRotFillView.kt
@@ -16,14 +16,49 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 90f
-val rot : Float = 90f
+val rot : Float = 270f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcRotFill(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
+        paint.style = Paint.Style.STROKE
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawArc(RectF(-size, -size / 2, 0f, size / 2), 180f, 180f * dsc(1), false, paint)
+        }
+        drawXY(-size, 0f) {
+            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        }
+        paint.style = Paint.Style.STROKE
+        drawArc(RectF(0f, 0f, size, size), 90f, 180f * dsc(3), true, paint)
+    }
+}
+
+fun Canvas.drawLARFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcRotFill(scale, w, h, paint)
+}
