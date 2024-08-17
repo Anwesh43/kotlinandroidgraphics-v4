9df5040928ae9f6ab0dfc94e3093dd9bc5a6679c
Anwesh43 <anweshthecool0@gmail.com>
2024-08-17 18:43:58 +0530

    linetriformrotview: adding Canvas LineTriFormRotView class

--- linetriformrotview/src/main/java/com/example/linetriformrotview/LineTriFormRotView.kt
+++ linetriformrotview/src/main/java/com/example/linetriformrotview/LineTriFormRotView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLTFRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineTriForm(scale, w, h, paint)
 }
+
+class LineTriFormRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
