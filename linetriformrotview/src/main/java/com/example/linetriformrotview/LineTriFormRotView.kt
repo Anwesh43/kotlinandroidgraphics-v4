88c991e91740eb1fa102ea0e346ba1eb90f18d93
Anwesh43 <anweshthecool0@gmail.com>
2024-08-19 12:20:58 +0530

    linetriformrotview: adding LTFRNode class

--- linetriformrotview/src/main/java/com/example/linetriformrotview/LineTriFormRotView.kt
+++ linetriformrotview/src/main/java/com/example/linetriformrotview/LineTriFormRotView.kt
@@ -124,4 +124,45 @@ class LineTriFormRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTFRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LTFRNode? = null
+        private var prev : LTFRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTFRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTFRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTFRNode? {
+            var curr : LTFRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
