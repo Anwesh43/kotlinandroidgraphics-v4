2412b27985d7d50eb79c34bc866c4f7886d43774
Anwesh43 <anweshthecool0@gmail.com>
2024-08-21 08:07:37 +0530

    bilinerotexpandview: adding BiLineRotExpandView class

--- bilinerotexpandview/src/main/java/com/example/bilinerotexpandview/BiLineRotExpandView.kt
+++ bilinerotexpandview/src/main/java/com/example/bilinerotexpandview/BiLineRotExpandView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawBLRENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineRotExpand(scale, w, h, paint)
 }
+
+class BiLineRotExpandView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
