e937d87d009a71fad6607a37482f105c182f194e
Anwesh43 <anweshthecool0@gmail.com>
2024-08-21 08:04:28 +0530

    bilinerotexpandview: adding Canvas extension methods

--- bilinerotexpandview/src/main/java/com/example/bilinerotexpandview/BiLineRotExpandView.kt
+++ bilinerotexpandview/src/main/java/com/example/bilinerotexpandview/BiLineRotExpandView.kt
@@ -27,3 +27,41 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiLineRotExpand(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2 + size) * dsc(3)) {
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(0f, 0f) {
+                    rotate(rot * dsc(1))
+                    drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+                }
+                drawXY(size, 0f) {
+                    drawLine(0f, 0f, 0f, size * dsc(2), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBLRENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLineRotExpand(scale, w, h, paint)
+}
