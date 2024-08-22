d25a0b2353fba582ea96b41aa5e551d8c2a91b7a
Anwesh43 <anweshthecool0@gmail.com>
2024-08-22 07:04:15 +0530

    linehalftofullarcview: adding constants and libraries

---
+++ linehalftofullarcview/src/main/java/com/example/linehalftofullarcview/LineHalfToFullArcView.kt
@@ -0,0 +1,25 @@
+package com.example.linehalftofullarcview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 90f
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
