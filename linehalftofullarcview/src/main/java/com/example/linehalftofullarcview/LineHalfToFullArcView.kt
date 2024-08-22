7616eeb4b883155100f58f51df77e6ecd312447d
Anwesh43 <anweshthecool0@gmail.com>
2024-08-22 07:30:28 +0530

    linehalftofullarcview: adding Canvas extension methods

--- linehalftofullarcview/src/main/java/com/example/linehalftofullarcview/LineHalfToFullArcView.kt
+++ linehalftofullarcview/src/main/java/com/example/linehalftofullarcview/LineHalfToFullArcView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 90f
@@ -27,3 +27,42 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineHalfToFullArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(rot * dsc(3))
+                drawXY(0f, 0f) {
+                    rotate(rot * dsc(1))
+                    drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+                }
+                drawXY(0f, 0f) {
+                    drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(2), false, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLHTFANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineHalfToFullArc(scale, w, h, paint)
+}
