0b1ffbaaf08624d82b02571ae009e9fbc48e5f85
Anwesh43 <anweshthecool0@gmail.com>
2024-08-22 07:43:14 +0530

    linehalftofullarcview: adding Renderer class

--- linehalftofullarcview/src/main/java/com/example/linehalftofullarcview/LineHalfToFullArcView.kt
+++ linehalftofullarcview/src/main/java/com/example/linehalftofullarcview/LineHalfToFullArcView.kt
@@ -191,4 +191,27 @@ class LineHalfToFullArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineHalfToFullArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val lhtfa : LineHalfToFullArc = LineHalfToFullArc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lhtfa.draw(canvas, paint)
+            animator.animate {
+                lhtfa.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lhtfa.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
