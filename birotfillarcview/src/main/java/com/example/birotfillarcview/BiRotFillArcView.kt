e34012253f0b48d16a87228963437d7169009c9e
Anwesh43 <anweshthecool0@gmail.com>
2024-08-24 09:52:41 +0530

    birotfillarcview: adding BiRotFillArc class

--- birotfillarcview/src/main/java/com/example/birotfillarcview/BiRotFillArcView.kt
+++ birotfillarcview/src/main/java/com/example/birotfillarcview/BiRotFillArcView.kt
@@ -171,4 +171,27 @@ class BiRotFillArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiRtoFillArc(var i : Int = 0) {
+
+        private var curr : BRFANode = BRFANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
