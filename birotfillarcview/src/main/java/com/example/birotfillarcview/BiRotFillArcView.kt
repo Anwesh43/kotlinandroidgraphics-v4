921e76922e0dcffbb3e71ff9c8ec925c39ec9126
Anwesh43 <anweshthecool0@gmail.com>
2024-08-24 09:43:11 +0530

    birotfillarcview: adding Canvas extension methods

--- birotfillarcview/src/main/java/com/example/birotfillarcview/BiRotFillArcView.kt
+++ birotfillarcview/src/main/java/com/example/birotfillarcview/BiRotFillArcView.kt
@@ -19,7 +19,7 @@ val colors : Array<String> = arrayOf(
 val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
-val rot : Float = 90f
+val rot : Float = 180f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,43 @@ val sizeFactor : Float = 4.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiRotFillArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        //stroked two arcs
+        paint.style = Paint.Style.STROKE
+        for (j in 0..1) {
+            drawXY(-size + size * j, 0f) {
+                rotate(rot * dsc(2) * j)
+                drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(j), false, paint)
+            }
+        }
+        //filled arc
+        paint.style = Paint.Style.FILL
+        drawXY(-size, 0f) {
+            drawArc(RectF(0f, -size / 2, size, size / 2), 0f, 360f * dsc(3), true, paint)
+        }
+
+
+    }
+}
+
+fun Canvas.drawBRFANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiRotFillArc(scale, w, h, paint)
+}
