3677baf897c79a9a9dcf933d623e3a99a70d818e
Anwesh43 <anweshthecool0@gmail.com>
2024-08-25 08:33:53 +0530

    lineexpandsqrightview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.birotfillarcview.BiRotFillArcView
+import com.example.lineexpandsqrightview.LineExpandSqRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        BiRotFillArcView.create(this)
+        LineExpandSqRightView.create(this)
     }
 }
