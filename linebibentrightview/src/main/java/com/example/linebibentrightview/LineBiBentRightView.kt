e67416267f5b11381f8651faf38177adae52980c
Anwesh43 <anweshthecool0@gmail.com>
2024-08-28 10:33:04 +0530

    linebibentrightview: adding LineBiBentRightView class

--- linebibentrightview/src/main/java/com/example/linebibentrightview/LineBiBentRightView.kt
+++ linebibentrightview/src/main/java/com/example/linebibentrightview/LineBiBentRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLBBRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBiBentRight(scale, w, h, paint)
 }
+
+class LineBiBentRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
