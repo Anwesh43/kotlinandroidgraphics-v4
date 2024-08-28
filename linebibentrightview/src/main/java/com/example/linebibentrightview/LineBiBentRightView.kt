24ee1dc973b126934f376af669a451211ed6a842
Anwesh43 <anweshthecool0@gmail.com>
2024-08-28 10:27:51 +0530

    linebibentrightview: adding Canvas extension methods

--- linebibentrightview/src/main/java/com/example/linebibentrightview/LineBiBentRightView.kt
+++ linebibentrightview/src/main/java/com/example/linebibentrightview/LineBiBentRightView.kt
@@ -27,3 +27,38 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBiBentRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            for (j in 0..1) {
+                drawXY(0f, 0f) {
+                    rotate(-deg * dsc(2) * j)
+                    drawLine(0f, 0f, -size * 0.5f * dsc(0), 0f, paint)
+                }
+            }
+        }
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+    }
+}
+
+fun Canvas.drawLBBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBiBentRight(scale, w, h, paint)
+}
