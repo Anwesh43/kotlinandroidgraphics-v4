0f5bebc1fc8653fa24677c314bf5c6586ad2cb43
Anwesh43 <anweshthecool0@gmail.com>
2024-08-28 10:56:39 +0530

    linebibentrightview: adding LineBiBentRight class

--- linebibentrightview/src/main/java/com/example/linebibentrightview/LineBiBentRightView.kt
+++ linebibentrightview/src/main/java/com/example/linebibentrightview/LineBiBentRightView.kt
@@ -166,4 +166,27 @@ class LineBiBentRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineBiBentRight(var i : Int) {
+
+        private var curr : LBBRNode = LBBRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
