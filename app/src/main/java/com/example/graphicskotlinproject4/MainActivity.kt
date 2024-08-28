75ea41f4521d4d6ea05477edeb7767b0ac2f37c0
Anwesh43 <anweshthecool0@gmail.com>
2024-08-28 11:01:26 +0530

    linebibentrightview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.lineexpandsqrightview.LineExpandSqRightView
+import com.example.linebibentrightview.LineBiBentRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        LineExpandSqRightView.create(this)
+        LineBiBentRightView.create(this)
     }
 }
