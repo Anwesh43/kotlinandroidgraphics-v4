e9fc627a6472be7132bbc48bbdc90df4b07f5370
Anwesh43 <anweshthecool0@gmail.com>
2024-08-29 10:36:52 +0530

    sqrotlineleftview: adding static create method

--- sqrotlineleftview/src/main/java/com/example/sqrotlineleftview/SqRotLineLeftView.kt
+++ sqrotlineleftview/src/main/java/com/example/sqrotlineleftview/SqRotLineLeftView.kt
@@ -211,4 +211,12 @@ class SqRotLineLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity: Activity) : SqRotLineLeftView {
+            val view : SqRotLineLeftView = SqRotLineLeftView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
