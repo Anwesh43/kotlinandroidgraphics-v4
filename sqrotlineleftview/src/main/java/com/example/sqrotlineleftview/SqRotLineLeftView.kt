a2a5e8901e92ccdd6d212f0de6f126b6244a1716
Anwesh43 <anweshthecool0@gmail.com>
2024-08-29 10:35:46 +0530

    sqrotlineleftview: adding Renderer class

--- sqrotlineleftview/src/main/java/com/example/sqrotlineleftview/SqRotLineLeftView.kt
+++ sqrotlineleftview/src/main/java/com/example/sqrotlineleftview/SqRotLineLeftView.kt
@@ -186,4 +186,27 @@ class SqRotLineLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SqRotLineLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val srll : SqRotLineLeft = SqRotLineLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            srll.draw(canvas, paint)
+            animator.animate {
+                srll.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            srll.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
