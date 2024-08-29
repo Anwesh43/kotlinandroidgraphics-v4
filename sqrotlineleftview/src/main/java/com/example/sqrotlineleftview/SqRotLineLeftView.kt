9163820170486eb16c60421ce7aec0d00f0aad4e
Anwesh43 <anweshthecool0@gmail.com>
2024-08-29 08:42:53 +0530

    sqrotlineleftview: adding SqRotLineLeftView class

--- sqrotlineleftview/src/main/java/com/example/sqrotlineleftview/SqRotLineLeftView.kt
+++ sqrotlineleftview/src/main/java/com/example/sqrotlineleftview/SqRotLineLeftView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawSRLLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSqRotLineLeft(scale, w, h, paint)
 }
+
+class SqRotLineLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
