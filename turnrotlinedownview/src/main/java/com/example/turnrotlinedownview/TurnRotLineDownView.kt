0fc1e9c1a4b5d336cc14140ff8464f375e93f428
Anwesh43 <anweshthecool0@gmail.com>
2024-08-30 09:13:46 +0530

    turnrotlinedownview: adding Canvas extension methods

--- turnrotlinedownview/src/main/java/com/example/turnrotlinedownview/TurnRotLineDownView.kt
+++ turnrotlinedownview/src/main/java/com/example/turnrotlinedownview/TurnRotLineDownView.kt
@@ -28,3 +28,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTurnRotLineDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(1))) {
+            rotate(rot * dsc(2))
+            drawRect(RectF(-size, -size, 0f, 0f), paint)
+        }
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+    }
+}
+
+fun Canvas.drawTRLDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawTurnRotLineDown(scale, w, h, paint)
+}
