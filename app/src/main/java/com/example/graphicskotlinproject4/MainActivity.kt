5bed48b1e6f1462110d2af0c9d47d547401f3621
Anwesh43 <anweshthecool0@gmail.com>
2024-08-30 09:38:52 +0530

    turnrotlinedownview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -10,13 +10,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sqrotlineleftview.SqRotLineLeftView
+import com.example.turnrotlinedownview.TurnRotLineDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        SqRotLineLeftView.create(this)
+        TurnRotLineDownView.create(this)
     }
 }
