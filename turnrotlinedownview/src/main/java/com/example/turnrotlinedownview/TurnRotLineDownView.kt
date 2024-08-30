05f991ca40a6cb400b064a72f69b27c1526a63a0
Anwesh43 <anweshthecool0@gmail.com>
2024-08-30 09:03:41 +0530

    turnrotlinedownview: adding Int and Float extension methods

---
+++ turnrotlinedownview/src/main/java/com/example/turnrotlinedownview/TurnRotLineDownView.kt
@@ -0,0 +1,30 @@
+package com.example.turnrotlinedownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 180f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
