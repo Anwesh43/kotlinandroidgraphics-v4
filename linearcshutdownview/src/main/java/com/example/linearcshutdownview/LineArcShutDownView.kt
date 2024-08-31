bd334de9a4f5747c388be6a15795714079f9d27d
Anwesh43 <anweshthecool0@gmail.com>
2024-08-31 10:13:19 +0530

    linearcshutdownview: adding LineArcShutDown class

--- linearcshutdownview/src/main/java/com/example/linearcshutdownview/LineArcShutDownView.kt
+++ linearcshutdownview/src/main/java/com/example/linearcshutdownview/LineArcShutDownView.kt
@@ -165,4 +165,27 @@ class LineArcShutDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineArcShutDown(var i : Int) {
+
+        private var curr : LASDNode = LASDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
