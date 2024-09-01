6beb3eeabb248bfd1e36d02b8e96708763a078d1
Anwesh43 <anweshthecool0@gmail.com>
2024-09-01 08:59:03 +0530

    arcrotagainlineview: adding ArcRotAgainLineView class

--- arcrotagainlineview/src/main/java/com/example/arcrotagainlineview/ArcRotAgainLineView.kt
+++ arcrotagainlineview/src/main/java/com/example/arcrotagainlineview/ArcRotAgainLineView.kt
@@ -60,3 +60,18 @@ fun Canvas.drawARALNode(i : Int, scale : Float, paint : Paint) {
     drawArcRotAgainLine(scale, w, h, paint)
 }
 
+class ArcRotAgainLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN ->  {
+
+            }
+        }
+        return true
+    }
+}
