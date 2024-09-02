bd7d2f09fd5a2f10a1c760d41ae292172227f1d8
Anwesh43 <anweshthecool0@gmail.com>
2024-09-02 08:53:13 +0530

    seqarctocircleview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -10,12 +10,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.arcrotagainlineview.ArcRotAgainLineView
+import com.example.seqarctocircleview.SeqArcToCircleView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        ArcRotAgainLineView.create(this)
+        SeqArcToCircleView.create(this)
     }
 }
