e6abf0cf8865518bc456a39c322d785357eba676
Anwesh43 <anweshthecool0@gmail.com>
2024-09-02 08:47:55 +0530

    seqarctocircleview: adding SATCNode class

--- seqarctocircleview/src/main/java/com/example/seqarctocircleview/SeqArcToCircleView.kt
+++ seqarctocircleview/src/main/java/com/example/seqarctocircleview/SeqArcToCircleView.kt
@@ -126,4 +126,45 @@ class SeqArcToCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SATCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SATCNode? = null
+        private var prev : SATCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SATCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSATCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SATCNode {
+            var curr : SATCNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
