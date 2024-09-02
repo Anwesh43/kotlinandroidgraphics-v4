497ddd6768a712fa0f7646fa3040da13cf374850
Anwesh43 <anweshthecool0@gmail.com>
2024-09-02 08:42:30 +0530

    seqarctocircleview: adding Animator class

--- seqarctocircleview/src/main/java/com/example/seqarctocircleview/SeqArcToCircleView.kt
+++ seqarctocircleview/src/main/java/com/example/seqarctocircleview/SeqArcToCircleView.kt
@@ -98,4 +98,32 @@ class SeqArcToCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animated(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
