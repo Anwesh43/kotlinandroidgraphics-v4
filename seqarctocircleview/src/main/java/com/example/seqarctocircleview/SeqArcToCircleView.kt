51537f3e047ac74bc6e5fece6e876e917ac5dbd4
Anwesh43 <anweshthecool0@gmail.com>
2024-09-02 08:40:08 +0530

    seqarctocircleview: adding SeqArcToCircleView class

--- seqarctocircleview/src/main/java/com/example/seqarctocircleview/SeqArcToCircleView.kt
+++ seqarctocircleview/src/main/java/com/example/seqarctocircleview/SeqArcToCircleView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawSATCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSeqArcToCircle(scale, w, h, paint)
 }
+
+class SeqArcToCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
