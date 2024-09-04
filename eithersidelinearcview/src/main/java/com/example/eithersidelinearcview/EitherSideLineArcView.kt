063d94fed1b87f63255b2e647864835aef3cb402
Anwesh43 <anweshthecool0@gmail.com>
2024-09-04 09:24:48 +0530

    eithersidelinearcview: adding EitherSideLineArcView class

--- eithersidelinearcview/src/main/java/com/example/eithersidelinearcview/EitherSideLineArcView.kt
+++ eithersidelinearcview/src/main/java/com/example/eithersidelinearcview/EitherSideLineArcView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawESLANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / sizeFactor
     drawEitherSideLineArc(scale, w, h, paint)
 }
+
+class EitherSideLineArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
