c575558e9097416de08c35425098d0ba7db8b5ea
Anwesh43 <anweshthecool0@gmail.com>
2024-09-04 09:42:55 +0530

    eithersidelinearcview: adding view in activity

--- app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
+++ app/src/main/java/com/example/graphicskotlinproject4/MainActivity.kt
@@ -10,12 +10,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.seqarctocircleview.SeqArcToCircleView
+import com.example.eithersidelinearcview.EitherSideLineArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        SeqArcToCircleView.create(this)
+        EitherSideLineArcView.create(this)
     }
 }
