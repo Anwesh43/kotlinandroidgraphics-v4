f9b350ce3b55d3a6f4a864bd59ba035c32542974
Anwesh43 <anweshthecool0@gmail.com>
2024-09-05 10:48:33 +0530

    linearcextendrotview: adding Canvas exension methods

--- linearcextendrotview/src/main/java/com/example/linearcextendrotview/LineArcExtendRotView.kt
+++ linearcextendrotview/src/main/java/com/example/linearcextendrotview/LineArcExtendRotView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val delay : Long = 20
@@ -27,3 +27,39 @@ val sizeFactor : Float = 4.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcExtendRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(-rot * dsc(2))
+            drawLine(0f, -size, 0f, size * (-1 + dsc(0)), paint)
+        }
+        paint.style = Paint.Style.STROKE
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawArc(RectF(-size / 2, 0f, size / 2, size), -90f, 180f * dsc(1), false, paint)
+        }
+        paint.style = Paint.Style.STROKE
+        drawArc(RectF(-size, -size / 2, 0f, size / 2), 0f, 180f * dsc(3), true, paint)
+    }
+}
+
+fun Canvas.drawLAERNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcExtendRot(scale, w, h, paint)
+}
